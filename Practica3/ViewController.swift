700d4945e4006396dd54459aa8a50004e1273583
USUARIO <usuario@F102-23.local>
2018-09-04 19:51:57 -0500

    Initial Commit

---
+++ Practica3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica3
+//
+//  Created by I104-21 on 04/09/18.
+//  Copyright © 2018 I104-21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
